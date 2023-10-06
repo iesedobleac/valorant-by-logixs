c20b8284dc1aa97d2a086c58db38934443c4100c
Isaac de los Reyes <isaacdelosredi@gmail.com>
2023-10-06 16:43:13 +0200

    Added hilt configuration for injecting dependencies

--- app/src/main/java/com/isaacdelosreyes/valorantforlogixs/MainActivity.kt
+++ app/src/main/java/com/isaacdelosreyes/valorantforlogixs/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.isaacdelosreyes.valorantforlogixs.navigation.NavigationHost
 import com.isaacdelosreyes.valorantforlogixs.ui.theme.ValorantForLogixsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
